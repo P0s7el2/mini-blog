d06818a0b3d2bf448a2b6bab63e6b00fa0d80363
P0s7el2 <romeo.com.ru@mail.ru>
2018-10-29 01:49:02 +0300

    access control Signed-off-by: P0s7el2 <romeo.com.ru@mail.ru>

--- app/core/Controller.php
+++ app/core/Controller.php
@@ -9,9 +9,13 @@ abstract class Controller {
     public $route;
     public $view;
     public $model;
+    public $acl;
 
     public function __construct($route) {
 		$this->route = $route;
+		if(!$this->checkAcl()){
+		    View::errorCode(403);
+        };
 		$this->view = new View($route);
 		$this->model = $this->loadModel($route['controller']);
 	}
@@ -21,6 +25,21 @@ abstract class Controller {
 	    if(class_exists($path)){
 	        return new $path;
         }
+    }
+
+    public function checkAcl(){
+        $this->acl = require 'app/acl/'.$this->route['controller'].'.php';
+        if($this->isAcl('all')){
+            return true;
+        } elseif (isset($_SESSION[admin][id]) and $this->isAcl('admin')){
+            return true;
+        }
+        return false;
+    }
+
+    public function isAcl($key)
+    {
+        return in_array($this->route['controller'], $this->acl[$key]);
     }
 
 }
